543913734edec37fdfb1fd76f5caf4d094b93a76
Sebas-Cortes <seb.cortes@duocuc.cl>
2022-04-26 21:18:01 -0400

    validacion 2

--- web/js/Validacion.js
+++ web/js/Validacion.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
     $("#ini").submit(function(){
-        e.preventDefault();
-    })
+        event.preventDefault();
+    });
+
+    $("#reg").submit(function(){
+        event.preventDefault();
+
+        let entrar = false;
+        var usu = $("#reg-usu").val();
+
+        if (usu.lenght<5 || usu.lenght>15){
+            $("#reg_usu").addClass("form-control is-valid");
+        }
+    });
+    $("#reg_usu").blur(function(){
+        
+    });
 })
