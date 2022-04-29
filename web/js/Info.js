b6eb6d016c5e3a5fd4a8d5091f3f09220dbbc67c
Sebas-Cortes <seb.cortes@duocuc.cl>
2022-04-29 19:41:30 -0400

    integracion de la api nashe

--- web/js/Info.js
+++ web/js/Info.js
@@ -1,26 +1,10 @@
 $(document).ready(function () {
     $("#btnMenuAvatar").click(function(){
-        var cont=0;
         $.get("https://valorant-api.com/v1/agents?language=es-ES", function(data){
+            let puesto = "";
             $.each(data.data ,function(i, item){
-                if(cont==0){
-                    $("#dropdown-menu-avatar").append('<div class row>'+
-                                                    '<div class="col-4 col-sm-3">'+
-                                                        '<a class="dropdown-item" href=""><img class="avatar" src="'+item.displayIconSmall+'"></a>'+
-                                                    '</div>'+
-                                                '');
-                }
-                if (cont<4 && cont!=0){
-                    $("#dropdown-menu-avatar").append('<div class="col-4 col-sm-3">'+
-                                                        '<a class="dropdown-item" href=""><img class="avatar" src="'+item.displayIconSmall+'"></a>'+
-                                                    '</div>');
-                }
-                if(cont>=4){
-                    $("#dropdown-menu-avatar").append('</div>')
-                    cont=0;
-                }
-                cont++;
-
+                puesto = "avatar"+i;
+                $("#"+puesto).html('<a class="dropdown-item" href=""><img class="avatar" src="'+item.displayIconSmall+'"></a>');
             });
         });
     });
