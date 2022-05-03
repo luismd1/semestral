7d7696151da38f59a4b7d31c63c890b509f6dbf0
Sebas-Cortes <seb.cortes@duocuc.cl>
2022-05-03 00:04:38 -0400

    Validaciones terminadas
    añañañañañañañañañañañañañañañañaña

--- web/js/Validacion.js
+++ web/js/Validacion.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function(e){
+    const formulario = document.getElementById("reg");
     const inputs = document.querySelectorAll('#reg input');
 
     const expresiones = {
         usuario: /^[a-zA-Z0-9\_\-]{5,20}$/, // Letras, numeros, guion y guion_bajo
         password: /^[A-Z][a-zA-Z0-9]{3,18}\.$/, // 4 a 12 digitos.
-        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, // todo separado por un @ y terminando con punto algo
+        correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ // todo separado por un @ y terminando con punto algo
     }
 
+    const campos = {
+        usu : false,
+        pass : false,
+        email : false
+    }
     const validacion = (e) => {
         switch (e.target.name){
             case "usuario" :
@@ -14,15 +20,17 @@ $(document).ready(function(){
             break;
             case "pass" :
                 validarCampo(expresiones.password, e.target, "pass");
+                coincidir("pass");
             break;
             case "pass2" :
                 coincidir("pass");
             break;
             case "email" :
                 validarCampo(expresiones.correo, e.target, "email");
+                coincidir("email");
             break;
             case "email2" :
-                
+                coincidir("email");
             break;
         }
     }
@@ -33,38 +41,45 @@ $(document).ready(function(){
             $("#reg-"+campo).addClass("is-valid");
             $("#msj-"+campo).removeClass("d-block");
             $("#msj-"+campo).addClass("d-none");
+            campos[campo] = true;
         }else{
             $("#reg-"+campo).removeClass("is-valid");
             $("#reg-"+campo).addClass("is-invalid");
             $("#msj-"+campo).removeClass("d-none");
             $("#msj-"+campo).addClass("d-block");
+            campos[campo] = false;
         }
     }
 
     const coincidir = (campo) =>{
-        const elemento = $("#reg-"+campo).value;
-        const elemento2 = $("#reg-"+campo+"2").value;
+        var pass1 = document.getElementById("reg-"+campo);
+        var pass2 = document.getElementById("reg-"+campo+"2");
 
-        if (elemento !== elemento2){
+        if (pass1.value !== pass2.value){
             $("#reg-"+campo+"2").removeClass("is-valid");
             $("#reg-"+campo+"2").addClass("is-invalid");
             $("#msj-"+campo+"2").removeClass("d-none");
             $("#msj-"+campo+"2").addClass("d-block");
+            campos[campo] = false;
         }else{
             $("#reg-"+campo+"2").removeClass("is-invalid");
             $("#reg-"+campo+"2").addClass("is-valid");
             $("#msj-"+campo+"2").removeClass("d-block");
             $("#msj-"+campo+"2").addClass("d-none");
+            campos[campo] = true;
         }
     }
-
     inputs.forEach((input) => {
         input.addEventListener('keyup', validacion);
         input.addEventListener('blur', validacion);
     });
 
-    function primeraLetra(texto){
-        letra = texto.charAt(0);
-        return letra == letra.toUpperCase();
-    }
+    $("#reg").submit(function(e){
+        e.preventDefault();
+        const termino = document.getElementById("terminos");
+        if(campos.usu && campos.pass && campos.email && termino.checked){
+            formulario.reset();
+
+        }
+    })
 });
